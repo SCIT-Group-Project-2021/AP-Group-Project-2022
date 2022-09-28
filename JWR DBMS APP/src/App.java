7f1c23ab4bb14bc7186e088881e6ded797da693b
Gabriel Tickle Garcia <67770813+LeAlmond@users.noreply.github.com>
2022-09-28 14:42:20 -0500

    Added a TODO comment and changed the default printout

--- JWR DBMS APP/src/App.java
+++ JWR DBMS APP/src/App.java
@@ -1,6 +1,8 @@
 
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        System.out.println("Welcome to the Jan’s Wholesale and Retail DBMS application Commandline");
+
+        //@todo TODO Teach everyone how to use the todo files
     }
 }
