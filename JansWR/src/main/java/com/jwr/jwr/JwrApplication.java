15d8dae60a88b7175f3224b589aec7af3fcf1b94
Gabriel Tickle Garcia <67770813+LeAlmond@users.noreply.github.com>
2022-09-28 18:31:00 -0500

    Spring Boot
    changed project to spring boot

---
+++ JansWR/src/main/java/com/jwr/jwr/JwrApplication.java
@@ -0,0 +1,13 @@
+package com.jwr.jwr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JwrApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JwrApplication.class, args);
+	}
+
+}
---
+++ JansWR/src/test/java/com/jwr/jwr/JwrApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jwr.jwr;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JwrApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
