dd008ca178f44a0a31f535457da783a9384e2ca1
Gabriel Tickle Garcia <67770813+LeAlmond@users.noreply.github.com>
2022-10-18 15:05:10 -0500

    re assesed the project based on the lecture Added hibernate 5 and log4j2 to the spring dependency list

---
+++ JNWR/src/main/java/JNWR/SystemApplication.java
@@ -0,0 +1,24 @@
+package JNWR;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SystemApplication {
+
+	private static final Logger logger = LogManager.getLogger(SystemApplication.class);
+	public static void main(String[] args) {
+		SpringApplication.run(SystemApplication.class, args);
+
+		logger.info("Test Info message");
+        logger.debug("Test Debug Message");
+        logger.error("Test Error message");
+        logger.trace("Test Trace message");
+        logger.fatal("Test Fatal message");
+        logger.warn("Test Warning message");
+        
+	}
+
+}
