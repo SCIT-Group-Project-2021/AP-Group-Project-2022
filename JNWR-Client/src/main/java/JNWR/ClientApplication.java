73ab33adece96cbbb1c83911cabe5715931f7247
Gabriel Tickle Garcia <67770813+LeAlmond@users.noreply.github.com>
2022-11-04 15:12:09 -0500

    Modified Pom to include persistence on client side Created Client Aspect of Server

--- JNWR-Client/src/main/java/JNWR/ClientApplication.java
+++ JNWR-Client/src/main/java/JNWR/ClientApplication.java
@@ -5,6 +5,8 @@ import org.apache.logging.log4j.Logger;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import Entity.*;
+import JNWR.Domain.*;
 import JNWR.application.posPage;
 
 @SpringBootApplication
@@ -18,14 +20,20 @@ public class ClientApplication {
         //TODO: Create client socket and have a way for it to connect to the server
         //TODO: Create UI for connecting client to server, after connecton s made make client login to access the rest of the database
 
-		logger.info("Test Info message");
+		/*logger.info("Test Info message");
         logger.debug("Test Debug Message");
         logger.error("Test Error message");
         logger.trace("Test Trace message");
         logger.fatal("Test Fatal message");
-        logger.warn("Test Warning message");
+        logger.warn("Test Warning message");*/
 
-        new posPage();
+        Client client = new Client();
+
+        Customer cust = new Customer("Gabriel","Tickle","2001-07-16","18765553606","gabe@gmail.com", "2022-11-03", "2023-11-03");
+
+        //client.sendCustomer(cust);
+        client.sendAction("shutDown");
+        //new posPage();
         
 	}
 
---
+++ JNWR-Client/src/main/java/JNWR/Domain/Client.java
@@ -0,0 +1,88 @@
+package JNWR.Domain;
+
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.net.Socket;
+
+import Entity.*;
+
+public class Client {
+
+    private ObjectInputStream objIs;
+    private static ObjectOutputStream objOs;
+    private Socket connectionSocket;
+    private String action = "";
+
+    public Client () {
+        this.createConnection();
+        this.configureStreams();
+        
+    }
+
+    public Client (String ip) {
+        this.createConnection(ip);
+        this.configureStreams();
+        
+    }
+
+    private void configureStreams() {
+        try {
+            objOs = new ObjectOutputStream(connectionSocket.getOutputStream());
+
+            objIs = new ObjectInputStream(connectionSocket.getInputStream());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void closeConnection() {
+        try {
+            objIs.close();
+            objOs.close();
+            connectionSocket.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void createConnection() {
+        try {
+            connectionSocket = new Socket("127.0.0.1",8888);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+    private void createConnection(String ip) {
+        try {
+            connectionSocket = new Socket(ip,8888);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    //region Utility Methods
+
+    public void sendAction(String action) {
+        this.action = action;
+        try {
+            objOs.writeObject(action);
+        } catch (IOException e) {
+            e.printStackTrace();
+
+        }
+    }
+
+    public void sendCustomer(Customer action) {
+        sendAction("addCustomer");
+        try {
+            objOs.writeObject(action);
+        } catch (IOException e) {
+            e.printStackTrace();
+
+        }
+    }
+
+    //endregion
+    
+}
